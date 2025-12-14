0d9c273f8d977cdb02ae06b5f915afd7f632368b
Student <student@d17-327e-pc-18.ipa.ii.agh.edu.pl>
2025-12-14 15:23:25 +0100

    Added message logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let title = 'Wall-E';
+  const [title, setTitle] = useState('Wall-E');
 
   function handleChange(event) {
-    console.log(event.target.value);
+    console.log(event.target.value)
+    setTitle(event.target.value);
   }
   
+  let message ='';
+  if (title.length <5){
+    message = "Title is too short!";
+  } else if (title.length <10){
+    message = "Title is OK!";
+  } else {
+    message = "Title is too long!";
+  }
+
   return (
     <div>
       <h1>My favourite movies to watch</h1>
+      <h2>My favourite movie for today is {title}</h2>
       <h2>My favourite movie for today is {title.toUpperCase()}</h2>
-      <input type="text" onChange={handleChange}/>
+      {
+        title.length > 0 && <div>{message}</div>
+      }
+      <input type="text" value={title} onChange={handleChange}/>
     </div>
   );
 }
